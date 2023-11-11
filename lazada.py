6bcbe54504fb7a1e99606984b9d5fd1c471b6f71
Phattharaphon Chamnonganuwat <36487935+JOMDIESEL@users.noreply.github.com>
2023-11-11 17:11:52 +0700

    Update lazada.py

--- lazada.py
+++ lazada.py
@@ -1,48 +1,45 @@
-#lazada.py
-
 from selenium import webdriver
 import time
 from bs4 import BeautifulSoup as soup
 
-opt = webdriver.ChromeOptions()
-opt.add_argument('headless')
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
 
+def check_price(url):
+    # เปิดเบราว์เซอร์ด้วย WebDriver
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    time.sleep(3)
 
-def checkprice(url):
-	driver = webdriver.Chrome(options=opt)
-	driver.get(url)
-	time.sleep(3)
+    # ดึง HTML จากหน้าเว็บ
+    page_html = driver.page_source
+    data = soup(page_html, 'html.parser')
+    
+    # ค้นหาข้อมูลสินค้าและราคา
+    products = data.findAll('div', {'class': 'c16H9d'})
+    prices = data.findAll('span', {'class': 'c13VH6'})
 
-	page_html = driver.page_source
-	#print(driver.page_source)
+    all_products = []
+    all_urls = []
+    all_prices = []
 
-	data = soup(page_html,'html.parser')
-	product = data.findAll('div',{'class':'c16H9d'})
-	price = data.findAll('span',{'class':'c13VH6'})
+    # วนลูปเพื่อดึงข้อมูลสินค้าและ URL
+    for product in products:
+        print(product.text)
+        print('https:' + product.a['href'])
 
-	all_product = []
-	all_url = []
-	all_price = []
+        all_products.append(product.text)
+        all_urls.append('https:' + product.a['href'])
 
-	for pd in product:
-		print(pd.text)
-		print('https:'+ pd.a['href'])
+    # วนลูปเพื่อดึงข้อมูลราคา
+    for price in prices:
+        price_text = price.text
+        price_text = price_text.replace('฿', '')
+        price_text = price_text.replace(',', '')
+        all_prices.append(float(price_text))
 
-		all_product.append(pd.text)
-		all_url.append('https:'+ pd.a['href'])
+    return all_products, all_urls, all_prices
 
-
-	for pc in price:
-		#print(pc.text)
-		pc_text = pc.text
-		pc_text = pc_text.replace('฿','')
-		pc_text = pc_text.replace(',','')
-		all_price.append(float(pc_text))
-
-	return (all_product,all_url,all_price)
-
-
+# URL ของหน้า Lazada ที่ต้องการตรวจสอบราคา
 url = 'https://www.lazada.co.th/shop-laptops/?spm=a2o4m.home.cate_1.3.1125515fmDrUwT'
-checkprice(url)
-
-
+check_price(url)
